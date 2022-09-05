43f4c10efc5b0b4d747b78ca24ac37eaa313dae7
igorhit <igor_hit@yahoo.com.br>
2022-09-04 22:18:53 -0300

    v0

---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './Button.css';
+
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'operation' : ''
+    classes += props.triple ? 'operation' : ''
+    
+    return (
+        <button
+            onClick={e => props.click && props.click(props.label)} 
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
+{/* export default props =>
+    <button className={`
+    button
+    ${props.operation ? 'operation' : ''}
+    ${props.double ? 'double' : ''}
+    ${props.triple ? 'triple' : ''}
+`}> */}
